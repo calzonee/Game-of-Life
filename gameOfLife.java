5c4d477a5b3c345db7e781234d5c4bb326674395
Leon Pablo <pablo.martinez@gmx.de>
2023-01-03 15:25:39 +0100

    Add gridRandom()
    Method to randomly fill grid with boolean values.

--- gameOfLife.java
+++ gameOfLife.java
@@ -15,8 +15,8 @@ public class gameOfLife {
 
     // create grid with pattern
     public static boolean[][] gridPattern(int row, int col) {
-        // 2D empty array
         boolean[][] grid = new boolean[row][col];
+        
         // adding the pattern
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
@@ -26,6 +26,19 @@ public class gameOfLife {
         }
         return grid; 
     }
+
+    // create random grid 
+    public static boolean[][] gridRandom(int  row, int col) {
+        boolean[][] grid = new boolean[row][col];
+
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[i].length; ++j) {
+                if (java.lang.Math.random() < 0.5)
+                    grid[i][j] = true;
+            }
+        }
+        return grid;
+    }// ToDo: find out how to code gridPattern() and gridRandom() into one method
 
     // Method to print a grid
     public static void printGrid(boolean[][] grid) {
@@ -37,9 +50,11 @@ public class gameOfLife {
     }
 
     // print next Generation
-    public static void printGenerations(boolean[][] grid, int generations) {
+    public static void printGenerations(boolean[][] grid, int x) {
         boolean[][] futureGen = nextGen(grid);
-        for (int i = 1; i <= generations; ++i) {
+
+        // x is the number of generations you want to print
+        for (int i = 1; i <= x; ++i) {
             System.out.println("_____________");
             System.out.println("Generation " + i + ":");
             printGrid(futureGen);
@@ -50,9 +65,11 @@ public class gameOfLife {
     // nextGen() contains rules of Life
     public static boolean[][] nextGen(boolean[][] grid) {
         boolean[][] futureGen = new boolean[grid.length][grid[0].length];
+
         // loop through every cell
         for (int iG = 0; iG < grid.length; ++iG) {         // rows
             for (int jG = 0; jG < grid[iG].length; ++jG) { // columns
+
                 // indexing through every neighbour iN of the cell
                 int countLife = 0;
                 for (int iN = -1; iN < 2; ++iN) {
@@ -87,14 +104,14 @@ public class gameOfLife {
         // Input
         int[] size = getUserInput();
 
-        // create grid with pattern
-        boolean[][] grid = gridPattern(size[0], size[1]);
+        // // create grid with pattern
+        boolean[][] grid = gridRandom(size[0], size[1]);
 
         // print grid with pattern (first generation)
         System.out.println("Generation 0:");
         printGrid(grid);
 
-        // nextGen
-        printGenerations(grid, 2);
+        // // nextGen
+        printGenerations(grid, 20);
     }
 }
